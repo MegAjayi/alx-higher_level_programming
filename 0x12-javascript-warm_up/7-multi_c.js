5648d26dc8e31ce171589ee7ebe4d4f0233161dd
MegAjayi <megajayi93@gmail.com>
2024-12-16 13:48:04 -0800

    well done

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,11 +4,9 @@ const args = process.argv;
 const convert = parseInt(args[2], 10);
 
 if (!isNaN(convert)) {
-	for (let i = 0; i < convert; i++)
-	{
-		console.log('C is fun');
-	}
-}
-else {
-	console.log('Missing number of occurrences');
+  for (let i = 0; i < convert; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
 }
