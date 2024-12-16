693d5e150067f0489ed43674ad15a00669fd927e
MegAjayi <megajayi93@gmail.com>
2024-12-16 05:15:40 -0800

    java

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-let fS = "C is fun";
-let SS = "Python is cool";
-let tS = "JavaScript is amazing";
+const fS = 'C is fun';
+const SS = 'Python is cool';
+const tS = 'JavaScript is amazing';
 
 console.log(fS);
 console.log(SS);
